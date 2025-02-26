65a944cf1991a0c0a3336d0a2502af1ba4bf6ab0
rehan-infomover <rehan.infomover@gmail.com>
2025-02-26 23:23:02 +0530

    initial commit

---
+++ src/main/java/com/mosquefinder/MosqueFinderApplication.java
@@ -0,0 +1,13 @@
+package com.mosquefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosqueFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosqueFinderApplication.class, args);
+	}
+
+}
